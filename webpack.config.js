c8d3663b3738c08cdb3de452546df5007b1cc876
Michael Wang <mwwpublic@gmail.com>
2016-09-18 18:47:14 -0400

    add vagrant and webpack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/, loader: 'babel',
+        exclude: /(node_modules)/
+      }
+    ]
+  },
+  devTool: 'source-map'
+};
