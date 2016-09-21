db5f439943b69b0ce408a00dc6689180fb6abaa6
Michael Wang <mwwpublic@gmail.com>
2016-09-20 22:34:09 -0400

    add initial home page

---
+++ lib/home/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import LoginForm from '../login/form';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <LoginForm />
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ lib/login/form.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function form() {
+  return (
+    <form>
+      <label>username
+        <input type="text" />
+      </label>
+      <label>password
+        <input type="password" />
+      </label>
+    </form>
+  );
+}
+
+export default form;
