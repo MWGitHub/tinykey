ab92b2916cfddffeb063e16451591af85988d92d
Michael Wang <mwwpublic@gmail.com>
2016-10-30 13:35:51 -0400

    add key to items

--- lib/components/directory.jsx
+++ lib/components/directory.jsx
@@ -3,13 +3,13 @@ import VaultConstants from '../constants/vault-constants';
 
 export default function Directory(props) {
   const root = props.root;
-  const output = root.map((data) => {
+  const output = root.map((data, i) => {
     if (data.type !== VaultConstants.TYPES.DIRECTORY) {
-      return <li>{data.name}</li>;
+      return <li key={i}>{data.name}</li>;
     }
 
     return (
-      <li>
+      <li key={i}>
         <p>{data.name}</p>
         <Directory root={data.children} />
       </li>
